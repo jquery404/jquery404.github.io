aacdb57710b9092f229dd82fcd3b914a49e3d042
jquery404 <1131600125@student.mmu.edu.my>
2016-01-16 17:07:11 +0800

    faisal

--- js/main.js
+++ js/main.js
@@ -9,8 +9,21 @@ $(function() {
     });
 	
 	
-	
-	
+	$(document).delegate('*[data-toggle="lightbox"]:not([data-gallery="navigateTo"])', 'click', function(event) {
+		event.preventDefault();
+		return $(this).ekkoLightbox({
+			onShown: function() {
+				if (window.console) {
+					return console.log('Checking our the events huh?');
+				}
+			},
+			onNavigate: function(direction, itemIndex) {
+				if (window.console) {
+					return console.log('Navigating '+direction+'. Current item: '+itemIndex);
+				}
+			}
+		});
+	});
 	
 	var myNavBar = {
 
