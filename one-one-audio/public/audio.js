a963b6e7c8ba81e5ef05721f59a7ff271256b9fd
jquery404 <jquery404@gmail.com>
2021-09-12 14:36:31 +1200

    one-one-audio viz

--- one-one-audio/public/audio.js
+++ one-one-audio/public/audio.js
@@ -1,8 +1,13 @@
 let localStream = null;
+let remoteStream = null;
 let peers = {}
+let audios = []
+let streams = []
+let colors = ['#EBC3DB', '#C4F7A1'];
 
 // Get camera and microphone
-const videoElement = document.querySelector("video");
+const hostAudio = document.querySelector(".host");
+const canvasEl = document.querySelector("canvas");
 const audioSelect = document.querySelector("select#audioSource");
 
 audioSelect.onchange = getStream;
@@ -31,9 +36,10 @@ function gotStream(stream) {
     audioSelect.selectedIndex = [...audioSelect.options].findIndex(
       option => option.text === stream.getAudioTracks()[0].label
     );
-    videoElement.srcObject = stream;
+    hostAudio.srcObject = stream;
     localStream = stream;
     init();
+    startVisualizer();
 }
 
 function handleError(error) {
@@ -99,6 +105,12 @@ function init(){
       console.log(socket_id, ' left')
       removePeer(socket_id)
     })
+
+    socket.on('full', () => {
+      var p = document.createElement("p"); 
+      p.innerHTML = "room is full";
+      document.body.appendChild(p); 
+    })
 }
 
 
@@ -108,6 +120,7 @@ function removePeer(socket_id) {
     peers[socket_id].onicecandidate = null; 
     delete peers[socket_id];
     document.querySelector("#"+socket_id).remove();
+    document.querySelector("#cvs"+socket_id).remove();
   }
 
   document.querySelector("pre").innerHTML += socket_id + ' left\n';
@@ -124,15 +137,24 @@ function addPeer(socket_id, am_initiator) {
     };
     
     peers.ontrack = event => {
-      if(!videos.includes(socket_id)){
-        let newVid = document.createElement('video')
-        newVid.srcObject = event.streams[0];
-        newVid.id = socket_id
-        newVid.playsinline = false
-        newVid.autoplay = true
-        newVid.muted = true
-        document.body.appendChild(newVid)
-        videos.push(socket_id);
+      if(!audios.includes(socket_id)){
+        let guestAudio = document.createElement('video')
+        remoteStream = event.streams[0];
+        guestAudio.srcObject = remoteStream;
+        guestAudio.id = socket_id
+        guestAudio.playsinline = false
+        guestAudio.autoplay = true
+        guestAudio.muted = true
+        document.body.appendChild(guestAudio)
+
+        let guestCanvasEl = document.createElement('canvas')
+        guestCanvasEl.width = 300;
+        guestCanvasEl.height = 50;
+        guestCanvasEl.id = 'cvs'+socket_id
+        document.body.appendChild(guestCanvasEl)
+
+        new AudioVisualizer(guestAudio, guestCanvasEl, colors[1], remoteStream).visualize();
+        audios.push(socket_id);
       }
         
 
@@ -151,3 +173,83 @@ function addPeer(socket_id, am_initiator) {
 
     return peers;
 }
+
+
+/** audio visualiser */
+function startVisualizer() {
+  new AudioVisualizer(hostAudio, canvasEl, colors[0], localStream).visualize();
+}
+
+function AudioVisualizer(video, canvas, color, stream) {
+  this.video = video;
+  this.canvas = canvas;
+  this.color = color;
+  this.stream = stream;
+}
+
+AudioVisualizer.prototype = {
+  async visualize() {
+    var canvas = this.canvas;
+    var WIDTH = canvas.width;
+    var HEIGHT = canvas.height;
+  
+    var ctx = canvas.getContext("2d");
+  
+    var audioContext = new (window.AudioContext || window.webkitAudioContext)();
+    var analyser = audioContext.createAnalyser();
+    var dataArray = new Uint8Array(analyser.frequencyBinCount);
+    
+    if (localStream instanceof Blob) {
+        const arrayBuffer = await new Response(this.stream).arrayBuffer();
+        const audioBuffer = await audioContext.decodeAudioData(arrayBuffer);
+        source = audioContext.createBufferSource();
+        source.buffer = audioBuffer;
+        source.connect(analyser);
+        source.start(0);
+    }
+    else {
+        var source = audioContext.createMediaStreamSource(this.stream);
+        source.connect(analyser);
+    }
+  
+    analyser.fftSize = 1024;
+    var bufferLength = analyser.fftSize;
+    var dataArray = new Uint8Array(bufferLength);
+  
+    ctx.clearRect(0, 0, WIDTH, HEIGHT);
+    
+    var draw = () => {
+        requestAnimationFrame(draw);
+        analyser.getByteTimeDomainData(dataArray);
+  
+        ctx.fillStyle = this.color;
+        ctx.fillRect(0, 0, WIDTH, HEIGHT);
+  
+        ctx.lineWidth = 2;
+        ctx.strokeStyle = "black";
+  
+        ctx.beginPath();
+  
+        var sliceWidth = WIDTH * 1.0 / bufferLength;
+        var x = 0;
+  
+        for(var i = 0; i < bufferLength; i++) {
+  
+            var v = dataArray[i] / 128.0;
+            var y = v * HEIGHT/2;
+  
+            if(i === 0)
+                ctx.moveTo(x, y);
+            else
+                ctx.lineTo(x, y);
+            x += sliceWidth;
+        }
+  
+        ctx.lineTo(WIDTH, HEIGHT/2);
+        ctx.stroke();
+    };
+  
+    draw();
+  }
+};
+
