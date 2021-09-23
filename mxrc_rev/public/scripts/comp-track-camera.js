127387d99f6a7298114d0d7dae2a43cec9fe9adb
Faisal Zaman <zamanfa@staff.vuw.ac.nz>
2021-09-23 14:26:52 +1200

    hand.glb

--- mxrc_rev/public/scripts/comp-track-camera.js
+++ mxrc_rev/public/scripts/comp-track-camera.js
@@ -1,14 +1,17 @@
 AFRAME.registerComponent('track-camera', {
     schema: {
         target: {default: []},
+        isTracking: {default: true},
     },
 
     init: function(){
         this.target = this.data.target;
+        this.isTracking = this.data.isTracking;
     },
 
     update: function(){
         this.target = this.data.target;
+        this.isTracking = this.data.isTracking;
     },
 
     tick: (function () {
@@ -17,24 +20,27 @@ AFRAME.registerComponent('track-camera', {
       const direction = new THREE.Vector3();
       const euler = new THREE.Euler();
       const matrix = new THREE.Matrix4();
-      return function () {
-        
-        this.el.object3D.updateMatrixWorld();
-        position.setFromMatrixPosition(this.el.object3D.matrixWorld); // Vector3
-        rotation.setFromRotationMatrix(matrix.extractRotation(this.el.object3D.matrixWorld ));
-        var e = this.el.object3D.matrixWorld.elements;
-        direction.set(e[8], e[9], e[10]).normalize();
-        
-        // var debug = document.getElementById("debug");
-        // debug.setAttribute("value", "Position: " + position.x.toFixed(2) + " " + position.y.toFixed(2) + " " + position.z.toFixed(2) + " ," + "Rotation: " + rotation.x.toFixed(2) + " " + rotation.y.toFixed(2) + " " + rotation.z.toFixed(2) + " ," + "Direction: " + direction.x.toFixed(2) + " " + direction.y.toFixed(2) + " " + direction.z.toFixed(2));
 
-        // target tracking
-        this.target.forEach((elem, i) => {
-            var targetEl = this.el.sceneEl.querySelector(elem);	
-            if (targetEl) {
-                this.lookAt(targetEl.object3D, position, i);
-            }
-        });
+      return function () {
+        if(this.isTracking){
+            this.el.object3D.updateMatrixWorld();
+            position.setFromMatrixPosition(this.el.object3D.matrixWorld); // Vector3
+            rotation.setFromRotationMatrix(matrix.extractRotation(this.el.object3D.matrixWorld));
+            var e = this.el.object3D.matrixWorld.elements;
+            direction.set(e[8], e[9], e[10]).normalize();
+            
+            // var debug = document.getElementById("debug");
+            // debug.setAttribute("value", "Position: " + position.x.toFixed(2) + " " + position.y.toFixed(2) + " " + position.z.toFixed(2) + " ," + "Rotation: " + rotation.x.toFixed(2) + " " + rotation.y.toFixed(2) + " " + rotation.z.toFixed(2) + " ," + "Direction: " + direction.x.toFixed(2) + " " + direction.y.toFixed(2) + " " + direction.z.toFixed(2));
+    
+            // target tracking
+            this.target.forEach((elem, i) => {
+                var targetEl = this.el.sceneEl.querySelector(elem);	
+                if (targetEl) {
+                    this.lookAt(targetEl.object3D, position, i);
+                }
+            });
+        }
+        
       }
     })(),
 
