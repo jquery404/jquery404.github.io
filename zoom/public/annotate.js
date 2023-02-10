4c661b4f694a8d858476aa21f7591225e2f44ce8
jquery404 <jquery404@gmail.com>
2023-02-11 09:19:22 +1300

    push

---
+++ zoom/public/annotate.js
@@ -0,0 +1,59 @@
+let c, ctx, x, y, prevX, prevY, currX, currY, isDrawing, width = 10, height = 10;
+
+function drawMain() {
+  c = document.querySelector("#annotate");
+  ctx = c.getContext("2d");
+  var ratio = window.devicePixelRatio ? window.devicePixelRatio : 1;
+  // original is 480 x 270
+  c.width = 240 * ratio;
+  c.height = 180 * ratio;
+
+  x = c.width / 2;
+  y = c.height / 2;
+
+  c.addEventListener("mousemove", (e) => {findxy('move', e)}, false);
+  c.addEventListener("mousedown", (e) => {findxy('down', e)}, false);
+  c.addEventListener("mouseup",   (e) => {findxy('up', e)}, false);
+  render_draw();
+}
+
+function findxy(res, e) {
+  if (res == 'down') {
+      prevX = currX;
+      prevY = currY;
+      currX = e.clientX - c.offsetLeft;
+      currY = e.clientY - c.offsetTop;
+
+      isDrawing = true;
+      ctx.beginPath();
+      ctx.fillStyle = 'red';
+      ctx.fillRect(currX, currY, 2, 2);
+      ctx.closePath();
+  }
+  if (res == 'up') {
+    isDrawing = false;
+  }
+  if (res == 'move' && isDrawing) {
+    prevX = currX;
+    prevY = currY;
+    currX = e.clientX - c.offsetLeft;
+    currY = e.clientY - c.offsetTop;
+    render_draw();
+  }
+}
+
+function render_draw() {
+  ctx.beginPath();
+  ctx.moveTo(prevX, prevY);
+  ctx.lineTo(currX, currY);
+  ctx.strokeStyle = 'red';
+  ctx.lineWidth = 5;
+  ctx.stroke();
+  ctx.closePath();
+}
+
+function clearCanvas() {
+  ctx.clearRect(0, 0, c.width, c.height);
+}
+
+drawMain();
---
+++ zoom/public/loadmodel.js
@@ -0,0 +1,51 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'OrbitControls';
+import { GLTFLoader } from 'GLTFLoader';
+import { RoomEnvironment } from 'RoomEnvironment';
+
+const lego = 'models/model1.glb';
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const renderer = new THREE.WebGLRenderer( { antialias: true } );
+renderer.setPixelRatio( window.devicePixelRatio );
+renderer.setSize( window.innerWidth*.5, window.innerHeight*.6 );
+renderer.outputEncoding = THREE.sRGBEncoding;
+const pmremGenerator = new THREE.PMREMGenerator( renderer );
+
+const scene = new THREE.Scene();
+scene.background = new THREE.Color( 0x111111 );
+scene.environment = pmremGenerator.fromScene( new RoomEnvironment(), 0.04 ).texture;
+
+document.getElementById("model_canvas").appendChild( renderer.domElement );
+
+camera.position.z = 5;
+
+const controls = new OrbitControls( camera, renderer.domElement );
+controls.target.set( 0, 0.5, 0 );
+controls.update();
+controls.enablePan = false;
+controls.enableDamping = true;
+
+
+const loader = new GLTFLoader();
+loader.load(lego, function ( gltf ) {
+	const model = gltf.scene;
+	model.position.set( 0, 0, 0 );
+	model.scale.set( 0.5, 0.5, 0.5 );
+	scene.add( model );
+}, undefined, function ( e ) {
+	console.error( e );
+} );
+
+const color = 0xFFFFFF;
+const intensity = 1;
+const light = new THREE.DirectionalLight(color, intensity);
+light.position.set(0, 10, 0);
+light.target.position.set(-5, 0, 0);
+scene.add(light);
+scene.add(light.target);
+
+function animate() {
+	requestAnimationFrame( animate );
+	renderer.render( scene, camera );
+};
+animate();
